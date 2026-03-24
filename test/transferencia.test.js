6de8fa947a54968354179e39ce336fbb4d8a134c
fabio-dev21 <fabiomoura.21@outlook.com>
2026-03-24 17:44:47 -0300

    Adicionando os fixtures para o corpo das requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,7 +1,8 @@
 const request = require('supertest');
 const {expect} = require('chai');
 require('dotenv').config();
-const { obterToken } = require('../helpers/autenticacao');     
+const { obterToken } = require('../helpers/autenticacao');
+const postTransferencias = require('../fixtures/postTransferencias.json')     
 
 describe('Trasferencias', ()=>{
     describe('POST/Transferencias',()=>{
@@ -10,25 +11,17 @@ describe('Trasferencias', ()=>{
            token = await obterToken('julio.lima',123456)
         })
         it('Deve retonar sucesso codigo 201 se a transferencia for maior ou igual a R$10:00', async () => {
-          
-           
-            const resposta = await request(process.env.BASE_URL)   
+          const bodyTransferencias = {...postTransferencias}
+           const resposta = await request(process.env.BASE_URL)   
             .post("/transferencias")
             .set('content-type','application/json')
             .set('Authorization','bearer '+token)
-            .send(  
-                {
-                    contaOrigem:3,
-                    contaDestino:2,
-                    valor: 11,
-                    token: ''
-                })
-            
-             expect(resposta.status).to.equal(201);
-             
-
+            .send(bodyTransferencias)
+            expect(resposta.status).to.equal(201);
         })
         it('Deve retonar sucesso codigo 422 se a transferencia for menor que R$10:00', async () => {
+           const bodyTransferencias = {...postTransferencias}
+           bodyTransferencias.valor = 7
            const token = await obterToken('julio.lima',123456)
             const resposta = await request('http://localhost:3000')   
             .post("/transferencias")
